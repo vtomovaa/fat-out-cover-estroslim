ee342c0866a7e27c14ab2b690c8ffca809575e8a
Victoria Tomova <victoriatomova55@gmail.com>
2024-11-15 09:33:04 +0200

    fat-out-cover-estroslim

---
+++ fat-out-cover-estroslim/js/faq.js
@@ -0,0 +1,4 @@
+function toggleAnswer(id) {
+    var answer = document.getElementById(id);
+    answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+}
---
+++ fat-out-cover-estroslim/js/swiper.js
@@ -0,0 +1,39 @@
+// const swiperEl = document.querySelector('swiper-container');
+// const swiper = swiperEl.swiper;
+
+// var appendNumber = 4;
+// var prependNumber = 1;
+// document
+//   .querySelector(".prepend-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide([
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>",
+//     ]);
+//   });
+// document
+//   .querySelector(".prepend-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide(
+//       '<swiper-slide>Slide ' + --prependNumber + "</swiper-slide>"
+//     );
+//   });
+// document
+//   .querySelector(".append-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.appendSlide(
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>"
+//     );
+//   });
+// document
+//   .querySelector(".append-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.appendSlide([
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
+//       '<swiper-slide>Slide ' + ++appendNumber + "</swiper-slide>",
+//     ]);
+//   });
